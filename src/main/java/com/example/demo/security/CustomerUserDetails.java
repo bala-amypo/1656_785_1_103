9f49dbfa2e13efaa85a58c71371c492e0b00f21c
bala-amypo <bala@amypo.in>
2025-12-28 06:47:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/CustomerUserDetails.java
@@ -0,0 +1,19 @@
+package com.example.demo.security;
+import com.example.demo.model.Employee;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+public class CustomUserDetails implements UserDetails {
+    private final Employee employee;
+    public CustomUserDetails(Employee employee){ this.employee=employee; }
+    @Override public Collection<? extends GrantedAuthority> getAuthorities(){ return List.of(new SimpleGrantedAuthority("ROLE_"+employee.getRole())); }
+    @Override public String getPassword(){ return ""; }
+    @Override public String getUsername(){ return employee.getEmail(); }
+    @Override public boolean isAccountNonExpired(){ return true; }
+    @Override public boolean isAccountNonLocked(){ return true; }
+    @Override public boolean isCredentialsNonExpired(){ return true; }
+    @Override public boolean isEnabled(){ return true; }
+    public Employee getEmployee(){ return employee; }
+}
