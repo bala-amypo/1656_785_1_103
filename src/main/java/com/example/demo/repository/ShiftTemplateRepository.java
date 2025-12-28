d28df215f2bd01a7ed348acea21963ee128af440
bala-amypo <bala@amypo.in>
2025-12-28 06:38:04 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/ShiftTemplateRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+import com.example.demo.model.ShiftTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+public interface ShiftTemplateRepository extends JpaRepository<ShiftTemplate,Long>{
+    Optional<ShiftTemplate> findByTemplateNameAndDepartment_Id(String templateName, Long deptId);
+    List<ShiftTemplate> findByDepartment_Id(Long deptId);
+}
